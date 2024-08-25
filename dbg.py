dc8b58e52da5c469e8c43f3733244d160024e476
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-08-24 17:53:04 -0700

    use ScrollView for disasm

--- dbg.py
+++ dbg.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+from rich.segment import Segment
 from textual.app import App, ComposeResult
 from textual.widget import Widget
+from textual.scroll_view import ScrollView
+from textual.geometry import Size
 from textual.widgets import Header, Footer, Static, Button
 from textual.reactive import reactive
-from textual.events import Resize
+from textual.events import Click
+from textual.strip import Strip
 
 from emulator_connector import EmulatorConnector
 
@@ -21,45 +25,88 @@ class Registers(Widget):
             text += f"{key}: {hex(value)}\n"
         return text
 
-class Disassembly(Widget):
-    def __init__(self, request_update):
-        super().__init__()
-        self.request_update = request_update
-
-    instructions = reactive([])
+class Disassembly(ScrollView):
     pc: reactive[int] = reactive(0)
-    height: reactive[int] = reactive(0)
+    mode: int
+    addr_mask: int
 
-    def on_mount(self) -> None:
-        # self.height = int(self.styles.height.value)
-        self.request_update()
-        pass
+    bp = "🔴"
+    muted_bp = "⭕"
 
-    def on_resize(self, event: Resize):
-        self.height = event.size.height
-        self.request_update()
-        pass
+    COMPONENT_CLASSES = {
+        "address",
+        "instruction-word",
+        "instruction-disasm",
+    }
 
-    def render(self) -> str:
-        disasm = ""
-        lines = 0
-        for addr, instr_bytes in self.instructions:
-            for i in md.disasm(instr_bytes, addr & 0xFFFFFFFF):
-                if addr == self.pc:
-                    disasm += "-> "
-                else:
-                    disasm += "   "
+    def __init__(self, mode = 32):
+        super().__init__()
+        self.mode = mode
+        self.addr_mask = 0xFFFFFFFF if mode == 32 else 0xFFFFFFFFFFFFFFFF
+        self.virtual_size = Size(0, (self.addr_mask + 1) >> 2)
 
-                disasm += f"{hex(addr & 0xFFFFFFFF)} {i.mnemonic} {i.op_str}\n"
-                lines += 1
+    def scroll_to_address(self, address):
+        address &= self.addr_mask
+        address >>= 2
+        self.scroll_to(y = address, animate = False)
 
-        return disasm
+    def scroll_to_address_if_needed(self, address):
+        address &= self.addr_mask
+        address >>= 2
+
+        _, scroll_y = self.scroll_offset
+        min_address = scroll_y
+        max_address = min_address + self.size.height
+
+        if address < min_address or address > max_address:
+            self.scroll_to(y = address, animate = False)
+
+    def scroll_to_pc(self):
+        self.scroll_to_address(self.pc)
+
+    def scroll_to_pc_if_needed(self):
+        self.scroll_to_address_if_needed(self.pc)
+
+    def on_click(self, click: Click):
+        _, scroll_y = self.scroll_offset
+        address = scroll_y + click.y - 1
+        address <<= 2
+        if click.x <= 8: # Only if you click on the address
+            self.notify(f"{hex(address)} {click.x}")
+
+    def render_line(self, y: int) -> Strip:
+        _, scroll_y = self.scroll_offset
+        address = (y + scroll_y) << 2
+
+        address_style = self.get_component_rich_style("address")
+        iw_style = self.get_component_rich_style("instruction-word")
+        disasm_style = self.get_component_rich_style("instruction-disasm")
+
+        segments = [Segment(("{:08X}" if self.mode == 32 else "{:016X}").format(address), address_style)]
+        segments.append(Segment(" -> " if address == (self.pc & self.addr_mask) else "    "))
+
+        try:
+            instr = int("0x" + emu.read_word(address), 16)
+            instr_bytes = instr.to_bytes(4, "big")
+            disasm = []
+            for i in md.disasm(instr_bytes, address & self.addr_mask):
+                disasm.append(f"{i.mnemonic} {i.op_str}")
+
+            segments.append(Segment("{:08X}".format(instr), iw_style))
+            if len(disasm) != 1:
+                segments.append(Segment(f"ERROR: len = {len(disasm)}"))
+            else:
+                segments.append(Segment("\t" + disasm[0], disasm_style))
+        except Exception:
+            segments.append(Segment("ERROR"))
+
+        return Strip(segments)
 
 class Status(Static):
     def compose(self) -> ComposeResult:
         self.styles.layout = "horizontal"
         yield Registers()
-        yield Disassembly(self.update_state)
+        yield Disassembly()
 
     def update_state(self) -> None:
         try:
@@ -67,21 +114,13 @@ class Status(Static):
             self.query_one(Registers).registers = registers
             self.query_one(Disassembly).pc = registers["pc"]
 
-            instrs = []
-            disasm_height = self.query_one(Disassembly).height
-            instrs_before_after = int((disasm_height - 1) // 2)
-
-            for addr in range(registers["pc"] - (instrs_before_after * 4), registers["pc"] + (instrs_before_after * 4), 4):
-                instr = int("0x" + emu.read_word(addr), 16)
-                instr_bytes = instr.to_bytes(4, "big")
-                instrs.append((addr, instr_bytes))
-
-            self.query_one(Disassembly).instructions = instrs
         except Exception as e:
             self.notify(f"Error updating state! {e}")
 
     def on_mount(self) -> None:
         emu.register_on_update_handler(self.update_state)
+        self.update_state()
+        self.query_one(Disassembly).scroll_to_pc_if_needed()
 
 
 class Controls(Static):
@@ -99,7 +138,8 @@ class DebuggerApp(App):
         ("c", "continue", "Continue"),
         ("b", "break", "Break"),
         ("q", "quit", "Quit Debugger"),
-        ("x", "quit_emulator", "Quit Emulator")
+        ("x", "quit_emulator", "Quit Emulator"),
+        ("p", "jump_to_pc", "Jump to PC"),
     ]
 
     def compose(self) -> ComposeResult:
@@ -122,6 +162,7 @@ class DebuggerApp(App):
     def action_step(self) -> None:
         try:
             emu.step()
+            self.query_one(Disassembly).scroll_to_pc_if_needed()
         except Exception as e:
             self.notify(f"Failed to step: {e}")
 
@@ -135,8 +176,15 @@ class DebuggerApp(App):
     def action_break(self) -> None:
         try:
             emu.brk()
+            self.query_one(Disassembly).scroll_to_pc_if_needed()
         except Exception as e:
             self.notify(f"Failed to break: {e}")
+
+    def action_jump_to_pc(self) -> None:
+        try:
+            self.query_one(Disassembly).scroll_to_pc_if_needed()
+        except Exception as e:
+            self.notify(f"Failed to jump to PC: {e}")
 
 if __name__ == "__main__":
     app = DebuggerApp()
